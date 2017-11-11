c1863ed4ebb385efe47fb9730894e1bcdc7cb257
Andres Morales <>
2017-11-11 09:55:09 -0500

    Ejemplo de Hilos

---
+++ src/UN/Hilos.java
@@ -0,0 +1,13 @@
+
+package UN;
+
+public class Hilos {
+
+    public static void main(String[] args) {
+        
+        Hilo hilo1 = new Hilo("Tarea 1");
+        Hilo hilo2 = new Hilo("Tarea 2");
+        
+    }
+    
+}
